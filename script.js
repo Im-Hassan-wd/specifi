a0052796fe1b0a54b53eb998355ca7e3bb10fbc4
Sodiq Taiwo <alhassansahihgmail.com>
2022-05-14 16:54:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const os = require('os');
+const fs = require('fs');
+console.log('1:', os.platform());
+console.log('2:', os.hostname())
+console.log('3:', os.release())
+console.log('4:', os.totalmem())
+console.log('5:', os.type())
+console.log('6:', os.freemem())
+console.log('7:', os.userInfo())
+console.log('8:', os.version())
+console.log('8:', os.uptime())
+console.log('9:', os.networkInterfaces())
+console.log('0:', os.loadavg())
+console.log('0:', os.endianness())
+console.log('0:', os.cpus())
+// console.log(os)
