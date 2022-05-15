12dc96a403c5427229594c7ef36ac50ae5ca89d4
Sodiq Taiwo <alhassansahihgmail.com>
2022-05-15 16:29:51 +0100

    getting specifications

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const os = require('os');
+
+console.log(os);
+
+// express app
+const app = express();
+
+// view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000, () => console.log('running on port 3000'));
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home', os: os });
+});
+
+app.get('/home', (req, res) => {
+    res.redirect('/');
+});
+
+// 404 page 
+// app.use((req, res) => {
+//     res.status(404).render();
+// });
